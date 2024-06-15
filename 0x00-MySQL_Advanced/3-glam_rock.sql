de73a1d1dc4f70147684e632066548c30a98b12c
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-06-15 23:23:33 +0100

    glam rock fansss

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,7 @@
 -- computing values in sql
 
-SELECT band_name, IFNULL(2022 - formed, 0) AS lifespan
+SELECT band_name,
+IFNULL(split - formed, 2022 - formed) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
