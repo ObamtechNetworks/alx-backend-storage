76e84da70e60956ce9ee9de0de80e013d828654f
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-08-14 01:32:36 +0100

    insert and return id

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Insert into mongodb document in a collection based on the kwargs"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """Inserts into a mongo_collection based on the kwargs"""
+
+    result = mongo_collection.insert_one(**kwargs)
+    if result:
+        return result._id
+    return []
