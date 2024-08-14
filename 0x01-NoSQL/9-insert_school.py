0e9770cd45ba43296e470b925cc8f524e76ceb70
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-08-14 01:34:41 +0100

    fixed bug

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -5,7 +5,7 @@
 def insert_school(mongo_collection, **kwargs):
     """Inserts into a mongo_collection based on the kwargs"""
 
-    result = mongo_collection.insert_one(**kwargs)
+    result = mongo_collection.insert_one({**kwargs})
     if result:
-        return result._id
+        return result.get('_id')
     return []
