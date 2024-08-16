f372db2b2cf7c9d17aa4a8d58170d45e3c303776
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-08-16 02:22:39 +0100

    fixed some bugs

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -14,7 +14,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str | bytes | int | float]) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """Store data into the cache (in -memory)
 
         Args:
